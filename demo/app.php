90fc099e1ec50ebf4aa47214f1ea1b482b898725
Anton Smirnov <sandfox@sandfox.me>
2024-12-16 21:14:45 +0200

    Put class in a namespace

--- demo/app.php
+++ demo/app.php
@@ -1,6 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 use Arokettu\ConsoleMenu\ConsoleMenuCommand;
+use Dummy\Dummy\DummyCommand;
 use Symfony\Component\Console\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
--- demo/dummy_command.php
+++ demo/dummy_command.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+namespace Dummy\Dummy;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
